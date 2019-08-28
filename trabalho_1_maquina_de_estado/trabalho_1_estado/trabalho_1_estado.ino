765aef8d8db533a251ff33a7ccdabffd1eac5086
lucas tavares <lucastavaresuerj2016@gmail.com>
2019-08-27 22:39:49 -0300

    update <Co-authored-by: Nobrinks pvictorcoelho@hotmail.com>

--- trabalho_1_maquina_de_estado/trabalho_1_estado/trabalho_1_estado.ino
+++ trabalho_1_maquina_de_estado/trabalho_1_estado/trabalho_1_estado.ino
@@ -2,14 +2,16 @@
 #define PIN_LED_OUT 11
 #define PIN_BUT_AC 2
 #define PIN_BUT_DC 4
+#define DELAY 100
 
 int but_ac = HIGH;// diz o estado b1
 int but_dc = HIGH;// diz o estado b2
 
 int estado = 1;
-int intervalo = 1000;
+int intervalo = 2000;
 int conta500 = 0;
 int contaTempo = 0;
+int contaFim = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,119 +22,163 @@ void setup() {
   
   digitalWrite(PIN_LED_OUT, HIGH);
   contaTempo = millis();
+  //Serial.begin(9600);
 }
 
 void loop() {
+  contaFim = millis();
   switch (estado) {
     case 1: //led_aceso
-      if(millis() - contaTempo > intervalo) { //intervalo
+      //Serial.print("-- estado 1 --\n");
+      if(contaFim - contaTempo > intervalo) { //intervalo
         contaTempo = millis();
         digitalWrite(PIN_LED_OUT, LOW);
-        estado = 2;
-      } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
-        intervalo = intervalo - 250;
-      } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
-        intervalo = intervalo + 250;
-      } else if(!digitalRead(PIN_BUT_AC)) { //pressiona b1        
+        estado = 2;        
+      } else if(!digitalRead(PIN_BUT_AC) && but_ac) { //pressiona b1        
         but_ac = LOW; //pressionado
+        //Serial.print("-- pressiona b1 --\n");
         estado = 3;
-      } else if(!digitalRead(PIN_BUT_DC)) { //pressiona b2
+      } else if(!digitalRead(PIN_BUT_DC) && but_dc) { //pressiona b2
         but_dc = LOW; //pressionado
+        //Serial.print("-- pressiona b2 --\n");
         estado = 6;
-      }  break;
+      } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
+        intervalo = intervalo - DELAY;
+        //Serial.print("-- soltar b1 --\n");
+        but_ac = HIGH;
+      } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
+        intervalo = intervalo + DELAY;
+        //Serial.print("-- soltar b2 --\n");
+        but_dc = HIGH;
+      } break;
       
     case 2: //led_apagado
-      if(millis() - contaTempo > intervalo) { //intervalo
+      //Serial.print("-- estado 2 --\n");
+      if(contaFim - contaTempo > intervalo) { //intervalo
         contaTempo = millis();
         digitalWrite(PIN_LED_OUT, HIGH);
         estado = 1;
-      } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
-        intervalo = intervalo - 250;
-      } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
-        intervalo = intervalo + 250;
-      } else if(!digitalRead(PIN_BUT_AC)) { //pressiona b1        
+      } else if(!digitalRead(PIN_BUT_AC) && but_ac) { //pressiona b1        
         but_ac = LOW; //pressionado
+        //Serial.print("-- pressiona b1 --\n");
         estado = 4;
-      } else if(!digitalRead(PIN_BUT_DC)) { //pressiona b2
+      } else if(!digitalRead(PIN_BUT_DC) && but_dc) { //pressiona b2
         but_dc = LOW; //pressionado
+        //Serial.print("-- pressiona b2 --\n");
         estado = 7;
-      }  break;
+      } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
+        intervalo = intervalo - DELAY;
+        //Serial.print("-- soltar b1 --\n");
+        but_ac = HIGH;
+      } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
+        intervalo = intervalo + DELAY;
+        //Serial.print("-- soltar b2 --\n");
+        but_dc = HIGH;
+      } break;
       
     case 3: //led_aceso
-      if(millis() - contaTempo > intervalo) { //intervalo
+      //Serial.print("-- estado 3 --\n");
+      if(contaFim - contaTempo > intervalo) { //intervalo
         contaTempo = millis();
         digitalWrite(PIN_LED_OUT, LOW);
         estado = 4;
+      } else if(!digitalRead(PIN_BUT_DC) && but_dc) { //pressiona b2
+        estado = 5;
+        //Serial.print("-- pressiona b2 --\n");
+        digitalWrite(PIN_LED_IN, HIGH);
       } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
-        intervalo = intervalo - 250;
+        intervalo = intervalo - DELAY;
+        //Serial.print("-- soltar b1 --\n");
         but_ac = HIGH;
         estado = 1;
       } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
-        intervalo = intervalo + 250;
+        intervalo = intervalo + DELAY;
+        //Serial.print("-- soltar b2 --\n");
         but_dc = HIGH;
-      } else if(!digitalRead(PIN_BUT_DC)) { //pressiona b2
-        estado = 5;
       } else if (millis() - conta500 > 500) { //500ms
         estado = 1;
+        conta500 = millis();
+        //Serial.print("-- 500ms --\n");
       } break;
 
     case 4: //led_apagado
-      if(millis() - contaTempo > intervalo) { //intervalo
+      //Serial.print("-- estado 4 --\n");
+      if(contaFim - contaTempo > intervalo) { //intervalo
         contaTempo = millis();
         digitalWrite(PIN_LED_OUT, HIGH);
         estado = 3;
+      } else if(!digitalRead(PIN_BUT_DC) && but_dc) { //pressiona b2
+        estado = 5;
+        //Serial.print("-- pressiona b1 --\n");
+        digitalWrite(PIN_LED_IN, HIGH);
       } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
-        intervalo = intervalo - 250;
+        intervalo = intervalo - DELAY;
+        //Serial.print("-- soltar b1 --\n");
         but_ac = HIGH;
         estado = 2;
       } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
-        intervalo = intervalo + 250;
+        intervalo = intervalo + DELAY;
+        //Serial.print("-- soltar b2 --\n");
         but_dc = HIGH;
-      } else if(!digitalRead(PIN_BUT_DC)) { //pressiona b2
-        estado = 5;
       } else if (millis() - conta500 > 500) { //500ms
         estado = 2;
+        //Serial.print("-- 500ms --\n");
+        conta500 = millis();
       } break;
 
     case 5: //manter_estado
+      //Serial.print("-- estado 5 --\n");
       while (true) {
         //fica aqui
       } break;
 
     case 6: //led_aceso
-      if(millis() - contaTempo > intervalo) { //intervalo
+      //Serial.print("-- estado 6 --\n");
+      if(contaFim - contaTempo > intervalo) { //intervalo
         contaTempo = millis();
         digitalWrite(PIN_LED_OUT, LOW);
         estado = 7;
+      } else if(!digitalRead(PIN_BUT_AC) && but_ac) { //pressiona b1
+        estado = 5;
+        digitalWrite(PIN_LED_IN, HIGH);
       } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
-        intervalo = intervalo - 250;
+        intervalo = intervalo - DELAY;
+        //Serial.print("-- soltar b1 --\n");
         but_ac = HIGH;
       } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
-        intervalo = intervalo + 250;
+        intervalo = intervalo + DELAY;
+        //Serial.print("-- soltar b2 --\n");
         but_dc = HIGH;
         estado = 1;
-      } else if(!digitalRead(PIN_BUT_AC)) { //pressiona b1
-        estado = 5;
       } else if (millis() - conta500 > 500) { //500ms
         estado = 1;
+        //Serial.print("-- 500ms --\n");
+        conta500 = millis();
       } break;
 
     case 7: //led_apagado
-      if(millis() - contaTempo > intervalo) { //intervalo
+      //Serial.print("-- estado 7 --\n");
+      if(contaFim - contaTempo > intervalo) { //intervalo
         contaTempo = millis();
         digitalWrite(PIN_LED_OUT, HIGH);
         estado = 6;
+      } else if(!digitalRead(PIN_BUT_AC) && but_ac) { //pressiona b1
+        estado = 5;
+        //Serial.print("-- pressiona b1 --\n");
+        digitalWrite(PIN_LED_IN, HIGH);
       } else if(digitalRead(PIN_BUT_AC) != but_ac) { //soltar b1
-        intervalo = intervalo - 250;
+        intervalo = intervalo - DELAY;
+        //Serial.print("-- soltar b1 --\n");
         but_ac = HIGH;
       } else if(digitalRead(PIN_BUT_DC) != but_dc) { //soltar b2
-        intervalo = intervalo + 250;
+        intervalo = intervalo + DELAY;
+        //Serial.print("-- soltar b2 --\n");
         but_dc = HIGH;
         estado = 2;
-      } else if(!digitalRead(PIN_BUT_AC)) { //pressiona b1
-        estado = 5;
       } else if (millis() - conta500 > 500) { //500ms
         estado = 2;
+        //Serial.print("-- 500ms --\n");
+        conta500 = millis();
       } break;
   }
 }
